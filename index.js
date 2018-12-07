b6c4a6cf99d603cdc63aea6b4a6f88e155b7b00e
Chris Cable <ccable827@yahoo.com>
2018-12-07 05:31:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// add solution here
+function theBeatlesPlay(muscians, instruments) {
+  for(var muscians = 0; muscians < muscians.length; count++) {
+    return muscians + 
+  }
+}
+
+
+
+var muscians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"]
+var instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"]
+
+var arrayLength = muscians.length;
+
+  for(var i = 0; i < arrayLength; i++) {
+    var muscian = muscians[i] 
+  }
+  
+  
+  
+  
+
+
+ + "plays " + instruments[i];
+
+
+
+
+var instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"]
+
+
+
+
+
+var myStringArray = ["Hello","World"];
+var arrayLength = myStringArray.length;
+for (var i = 0; i < arrayLength; i++) {
+    alert(myStringArray[i]);
+    //Do something
+}
